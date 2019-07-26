d4858b5149114c3a53414327cdaffabf8b6070fe
ayshamarty <ayshamarty@gmail.com>
2019-07-26 09:44:07 +0100

    post working with hashing and email validations

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const Users = require("../models/userMod");
+const validator = require("../validators/userVal");
+const bcrypt = require("bcrypt");
 
 // @routes GET user/getAll
 // @desc Get all users
@@ -19,6 +21,33 @@ router.get("/getAll", (req, res) => {
         res.status(404).json(errors);
       });
   });
+
+
+  router.post("/create", (req, res) => {
+    const user = new Users({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
+  
+    const response = validator.userVal(user);
+  
+    if (response.isValid) {
+      payload = {};
+  
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          if (err) throw err;
+          user.password = hash;
+          user.save();
+          res.status(200).json({ message: "User added" });
+        });
+      });
+    } else {
+      res.send(response.errors);
+    }
+  });
+
 
 
 
