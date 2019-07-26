b0b96bb83fc5580d5d6b9eb0cd2c0120481bd5c1
ayshamarty <ayshamarty@gmail.com>
2019-07-26 09:56:51 +0100

    crud working but now need to make sure password is rehashed and email reverified before update

--- routes/user.js
+++ routes/user.js
@@ -10,45 +10,84 @@ const bcrypt = require("bcrypt");
 // @access Public
 
 router.get("/getAll", (req, res) => {
-    const errors = {};
-  
-    Users.find()
-      .then(Users => {
-        res.json(Users);
-      })
-      .catch((err) => {
-        errors.findItems = err;
-        res.status(404).json(errors);
-      });
-  });
+  const errors = {};
 
-
-  router.post("/create", (req, res) => {
-    const user = new Users({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
+  Users.find()
+    .then(Users => {
+      res.json(Users);
+    })
+    .catch(err => {
+      errors.findUsers = err;
+      res.status(404).json(errors);
     });
-  
-    const response = validator.userVal(user);
-  
-    if (response.isValid) {
-      payload = {};
-  
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(req.body.password, salt, (err, hash) => {
-          if (err) throw err;
-          user.password = hash;
-          user.save();
-          res.status(200).json({ message: "User added" });
-        });
+});
+
+// @routes POST user/create
+// @desc Create user
+// @access Public
+
+router.post("/create", (req, res) => {
+  const user = new Users({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  const response = validator.userVal(user);
+
+  if (response.isValid) {
+    payload = {};
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) throw err;
+        user.password = hash;
+        user.save();
+        res.status(200).json({ message: "User added" });
       });
-    } else {
-      res.send(response.errors);
-    }
+    });
+  } else {
+    res.send(response.errors);
+  }
+});
+
+// @routes Delete user/delete
+// @desc Delete user by ID
+// @access Public
+
+router.delete("/deleteID", (req, res) => {
+  const errors = {};
+
+  Users.deleteOne({
+    _id: req.body.id
+  })
+    .then(({ ok, n }) => {
+      if (n === 0) {
+        errors.noDelete = "user not deleted";
+        res.status(404).json(errors);
+      }
+      res.status(200).json({ message: "user deleted" });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+
+// @routes PUT user/update
+// @desc Update user
+// @access Public
+
+router.put("/update", (req, res) => {
+    Users.updateOne(
+      { username: req.body.username },
+      { $set: { email: req.body.email } },
+      { $set: { password: req.body.password } }
+    )
+      .then(() => res.status(200).json({ message: "user updated" }))
+      .catch(err => res.send(err));
   });
 
 
 
-
-  module.exports = router;
+module.exports = router;
